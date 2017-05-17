7bc72cab0a5dd6309aa10f15e6691ed09ba266c7
Remco Zwartepoorte <remco.zwartepoorte@gmail.com>
2017-05-17 10:26:17 +0200

    added js and css files

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    //getQuote();
+    //$("#newQuote").on("click", getQuote);
+    //$("#newTweet").on("click", tweetQuote);
+    //document.getElementById("quote-text").style.color = "blue";
+});
