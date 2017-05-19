bbf6c06f19f4470eb9f3a525d3d373872a02022c
Remco Zwartepoorte <remco.zwartepoorte@gmail.com>
2017-05-19 19:24:51 +0200

    Finished!

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-/*
-var x = document.getElementById("text");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
-
-function getWeatherInfo() {
-  $.ajax({
-    url: "https://api.darksky.net/forecast/c0e823d8c84f968c97291b5c57af2288/" + position.coords.latitude + "," + position.coords.longtitude,
-    success: function (json) {
-      console.log(json);
-      $("#locationCity").html(json.timezone);
-      //$("#quote-author").html("- " + post.title);
-    },
-    cache: false
-  });
-}
-
-$(document).ready(function () {
-  getLocation();
-  $("#find-me").on("click", getWeatherInfo);
-});
-*/
-
 $(document).ready(function () {
   /*--------------------------------------------------------------
   # Define global variables
@@ -39,6 +5,7 @@ $(document).ready(function () {
   var weatherData, temp, wind, icon;
   var lat = 42;
   var lng = 41;
+  var measurement = "imperial";
   var baseURL = "https://api.darksky.net/forecast/c0e823d8c84f968c97291b5c57af2288/";
 
   /*--------------------------------------------------------------
@@ -87,19 +54,46 @@ $(document).ready(function () {
   --------------------------------------------------------------*/
   function showWeather() {
     $("#location").html(weatherData.timezone);
+    temp = weatherData.currently.temperature
     $("#temp").html(weatherData.currently.temperature + "&deg;F");
     $("#currentWeather").html(weatherData.currently.summary);
-
     var icon = weatherData.currently.icon;
-	  // Add Sky Icon
-		var skycons = new Skycons({"color": "black"});
+    // Add Sky Icon
+    var skycons = new Skycons({
+      "color": "black"
+    });
+    // Add canvas by ID
+    skycons.add("weather-icon", icon);
+    // Start animation!
+    skycons.play();
+  }
 
-		// Add canvas by ID
-		skycons.add("weather-icon", icon);
-
-		// Start animation!
-		skycons.play();
-
-
-}
+  /*--------------------------------------------------------------
+  # switchDegrees()
+  --------------------------------------------------------------*/
+  function convert() {
+    if (measurement === "imperial") { // If 'murica measurements
+      measurement = "metric"; // Change to metric
+      temp = (temp - 32) * (5 / 9); // Change to Celsius ( c = f - 32 *(5/9))
+      $("#temp").fadeOut(300, function () { //Neat fade effect
+        $("button>span").html("F"); // Make the button say "Show in °F"
+        $("#temp").html(Math.round(temp) + "&deg;C"); // Display temperature as `XX°C`
+        $("#temp").fadeIn(300); // Fade it in
+      });
+    } else { //Otherwise it's metric
+      measurement = "imperial"; // Change to American / Imperial measurements
+      temp = temp * 9 / 5 + 32; // Change to Fahrenheit (F = C * 9 / 5 + 32)
+      $("#temp").fadeOut(300, function () {
+        $("button>span").html("C"); // Make the button say "Show in °C"
+        $("#temp").html(Math.round(temp) + "&deg;F"); // Display temperature as `XX°F`
+        $("#temp").fadeIn(300); // Fade them in
+      });
+    }
+  }
+  /*--------------------------------------------------------------
+  ## Convert button click handler | When you click the "Show in °X" button
+  --------------------------------------------------------------*/
+  $("#switch").click(function () {
+    convert();
+  });
 });
