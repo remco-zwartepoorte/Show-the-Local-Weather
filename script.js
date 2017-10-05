ae9251cd2187086f7bf2f58c0fb0441cb7056e62
Remco Zwartepoorte <remco.zwartepoorte@gmail.com>
2017-10-05 12:55:18 +0200

    add cors-anywhere workaround

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
   getLocation();
 });
 
-document.getElementById("switchtemp").addEventListener('click', convertTemp);
+document.getElementById('switchtemp').addEventListener('click', convertTemp);
 // document.getElementById("loadweather").addEventListener('click', loadWeather);
 
 var weatherData, temp, wind, icon, fetchURL, skycons;
 var lat = 42;
 var lng = 41;
-var measurement = "imperial";
-var baseURL = "https://api.darksky.net/forecast/c0e823d8c84f968c97291b5c57af2288/";
-
+var measurement = 'imperial';
+var baseURL =
+  'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/c0e823d8c84f968c97291b5c57af2288/';
 
 // function loadWeather() {
 //   getWeather();
@@ -19,65 +19,63 @@ var baseURL = "https://api.darksky.net/forecast/c0e823d8c84f968c97291b5c57af2288
 // };
 
 function getLocation() {
-  navigator.geolocation.getCurrentPosition(function (position) {
+  navigator.geolocation.getCurrentPosition(function(position) {
     lat = position.coords.latitude;
     lng = position.coords.longitude;
-    getWeather()
-  })
+    getWeather();
+  });
 }
 
 function getWeather() {
-  var fetchURL = baseURL + lat + "," + lng;
+  var fetchURL = baseURL + lat + ',' + lng;
   fetch(fetchURL, {
-    cache: "reload"
+    cache: 'reload'
   })
-    .then(
-    function (response) {
+    .then(function(response) {
       if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        console.log(
+          'Looks like there was a problem. Status Code: ' + response.status
+        );
         return;
       }
       // Examine the text in the response
-      response.json().then(function (data) {
+      response.json().then(function(data) {
         weatherData = data;
         console.log(data);
         showWeather();
         // document.getElementById("switchtemp").style.display = "inline";
-
       });
-    }
-    )
-    .catch(function (err) {
+    })
+    .catch(function(err) {
       console.log('Fetch Error :-S', err);
     });
-
 }
 
 function showWeather() {
-  document.getElementById("location").innerHTML = weatherData.timezone;
-  temp = weatherData.currently.temperature
-  document.getElementById("temp").innerHTML = weatherData.currently.temperature; //+ "&deg;F";
-  document.getElementById("currentWeather").innerHTML = weatherData.currently.summary;
+  document.getElementById('location').innerHTML = weatherData.timezone;
+  temp = weatherData.currently.temperature;
+  document.getElementById('temp').innerHTML = weatherData.currently.temperature; //+ "&deg;F";
+  document.getElementById('currentWeather').innerHTML =
+    weatherData.currently.summary;
   var icon = weatherData.currently.icon;
   var skycons = new Skycons({
-    "color": "black"
+    color: 'black'
   });
-  skycons.add("weather-icon", icon);
+  skycons.add('weather-icon', icon);
   skycons.play();
   convertTemp();
 }
 
 function convertTemp() {
-  if (measurement === "imperial") {
-    measurement = "metric";
+  if (measurement === 'imperial') {
+    measurement = 'metric';
     temp = (temp - 32) * (5 / 9);
-    document.getElementById("switchtemp").innerHTML = "Show in &degF";
-    document.getElementById("temp").innerHTML = Math.round(temp) + "&deg;C";
-  }
-  else {
-    measurement = "imperial";
+    document.getElementById('switchtemp').innerHTML = 'Show in &degF';
+    document.getElementById('temp').innerHTML = Math.round(temp) + '&deg;C';
+  } else {
+    measurement = 'imperial';
     temp = temp * 9 / 5 + 32;
-    document.getElementById("switchtemp").innerHTML = "Show in &degC";
-    document.getElementById("temp").innerHTML = Math.round(temp) + "&deg;F";
+    document.getElementById('switchtemp').innerHTML = 'Show in &degC';
+    document.getElementById('temp').innerHTML = Math.round(temp) + '&deg;F';
   }
 }
